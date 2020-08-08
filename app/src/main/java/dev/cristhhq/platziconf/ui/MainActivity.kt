9101e8cbb38c3e27a9553b5c3f495fe5137eb415
cristhian ochoa <cristhho@gmail.com>
2020-08-08 17:40:38 -0500

    set up navigation

--- app/src/main/java/dev/cristhhq/platziconf/ui/MainActivity.kt
+++ app/src/main/java/dev/cristhhq/platziconf/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package dev.cristhhq.platziconf.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import dev.cristhhq.platziconf.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setActionBar(toolbar)
+        configNav()
+    }
+
+    private fun configNav() {
+        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragContent))
     }
 }
